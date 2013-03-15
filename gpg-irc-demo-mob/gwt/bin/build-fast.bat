f787cd7472b5c33c6da544e0867704ff2544ebb3
demarco.m73 <demarco.m73@gmail.com>
2013-03-15 15:36:55 +0000

    VERSIONE PRIMA DELL'INTRODUZIONE DI Article, ArticleFolder, ArticleFolderPage etc...

--- gpg-irc-demo-mob/gwt/bin/build-fast.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-set APPNAME=gpg-irc-demo-gwt
-call %~dp0\build-base.bat compile gwt:compile war:exploded assembly:single
--- gpg-template/gwt/bin/build-fast.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-
-set APPNAME=gpg-demo-gwt
-call %~dp0\build-base.bat compile gwt:compile war:exploded assembly:single
