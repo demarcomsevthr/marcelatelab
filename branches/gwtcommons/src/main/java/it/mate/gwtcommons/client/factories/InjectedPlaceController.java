16bd1cf2d8e224a4d821e5add52c6d8ddc7002c9
demarco.m73@gmail.com <demarco.m73@gmail.com@97c78bc8-0749-d6cc-9a0f-1e4a5857900f>
2014-08-07 07:28:09 +0000

    GWT 2.6.x

---
+++ branches/gwtcommons/src/main/java/it/mate/gwtcommons/client/factories/InjectedPlaceController.java
@@ -0,0 +1,14 @@
+package it.mate.gwtcommons.client.factories;
+
+import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.place.shared.PlaceController;
+import com.google.inject.Inject;
+
+public class InjectedPlaceController extends PlaceController {
+
+  @Inject
+  public InjectedPlaceController(EventBus eventBus) {
+    super(eventBus);
+  }
+  
+}
