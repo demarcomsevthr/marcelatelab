c2803aa596a87e4769964a083d4d40fe25c28edd
demarco.m73 <demarco.m73@gmail.com>
2015-11-09 11:45:59 +0100

    .

--- onscommons/src/main/java/it/mate/onscommons/client/ui/OnsDateBox.java
+++ onscommons/src/main/java/it/mate/onscommons/client/ui/OnsDateBox.java
@@ -3,9 +3,6 @@ package it.mate.onscommons.client.ui;
 import it.mate.gwtcommons.client.utils.Delegate;
 import it.mate.gwtcommons.client.utils.GwtUtils;
 import it.mate.gwtcommons.client.utils.JQuery;
-import it.mate.onscommons.client.event.NativeGestureEvent;
-import it.mate.onscommons.client.event.NativeGestureHandler;
-import it.mate.onscommons.client.event.OnsEventUtils;
 import it.mate.onscommons.client.onsen.OnsenUi;
 import it.mate.onscommons.client.utils.OnsDatePicker;
 import it.mate.phgcommons.client.utils.OsDetectionUtils;
@@ -14,10 +11,16 @@ import it.mate.phgcommons.client.utils.PhgUtils;
 import java.util.Date;
 
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.user.client.Timer;
 
 
 public class OnsDateBox extends OnsTextBox {
+  
+  /**
+   * REVISION DATE 09/11/2015
+   * 
+   * > ios test upgrades
+   * 
+   */
   
 
   // 02/11/2015
@@ -35,18 +38,16 @@ public class OnsDateBox extends OnsTextBox {
       */
   
   
-  private static boolean applyAndroidPatch1 = false;
-  
-  private static boolean applyAndroidPatch2 = false;
-  
-  private static String JS_FORMAT = "dd/MM/yyyy";
+  private static String JS_DATEPICKER_FORMAT = "dd/MM/yyyy";
   
   private String actualFormat = "dd/MM/yyyy";
+  
+  private static String actualInputType = (USE_ONS_DATEPICKER ? "text" : "date");
   
   private String language;
   
   public OnsDateBox() {
-    super( USE_ONS_DATEPICKER ? "text" : "date");
+    super( actualInputType );
     language = PhgUtils.getAppLocalLanguage();
     if ("it".equalsIgnoreCase(language)) {
       actualFormat = "dd/MM/yyyy";
@@ -54,10 +55,9 @@ public class OnsDateBox extends OnsTextBox {
       actualFormat = "yyyy/MM/dd";
     }
     addStyleName("text-input");
-    PhgUtils.log("OnsDateBox: USE_JS_DATEPICKER=" + USE_JS_DATEPICKER + " USE_ONS_DATEPICKER="+USE_ONS_DATEPICKER + " language="+language + " actualFormat="+actualFormat);
+    PhgUtils.log("OnsDateBox: USE_JS_DATEPICKER=" + USE_JS_DATEPICKER + " USE_ONS_DATEPICKER="+USE_ONS_DATEPICKER + " language="+language + " actualFormat="+actualFormat + " actualInputType=" + actualInputType);
     if (USE_JS_DATEPICKER) {
       initializeDatepicker();
-      androidPatch2();
     } else if (USE_ONS_DATEPICKER) {
       OnsenUi.onAvailableElement(this, new Delegate<Element>() {
         public void execute(Element element) {
@@ -65,6 +65,10 @@ public class OnsDateBox extends OnsTextBox {
         }
       });
     }
+  }
+
+  public static String getActualInputType() {
+    return actualInputType;
   }
   
   public Date getValueAsDate() {
@@ -74,7 +78,7 @@ public class OnsDateBox extends OnsTextBox {
     }
     Date result = null;
     if (USE_JS_DATEPICKER) {
-      result = GwtUtils.stringToDate(text, JS_FORMAT);
+      result = GwtUtils.stringToDate(text, JS_DATEPICKER_FORMAT);
     } else {
       String tryFormat = "yyyy-MM-dd";
       result = tryParseDate(tryFormat);
@@ -109,14 +113,13 @@ public class OnsDateBox extends OnsTextBox {
     super.setValue(value, fireEvents);
     if (USE_JS_DATEPICKER) {
       setDataValue(value);
-    } else if (USE_ONS_DATEPICKER) {
-      final String fValue = value;
-      OnsenUi.onAvailableElement(this, new Delegate<Element>() {
-        public void execute(Element element) {
-          element.setInnerText(fValue);
-        }
-      });
     }
+    final String fValue = value;
+    OnsenUi.onAvailableElement(this, new Delegate<Element>() {
+      public void execute(Element element) {
+        element.setInnerText(fValue);
+      }
+    });
   }
 
   public void setDataValue(final String dataValue) {
@@ -133,72 +136,13 @@ public class OnsDateBox extends OnsTextBox {
       public void execute(Void element) {
         OnsenUi.onAvailableElement(OnsDateBox.this, new Delegate<Element>() {
           public void execute(Element element) {
-            androidPatch1();
             String containerId = null;
-            JQuery.withElement(element).pickdate(JS_FORMAT.toLowerCase(), containerId, true, true);
+            JQuery.withElement(element).pickdate(JS_DATEPICKER_FORMAT.toLowerCase(), containerId, true, true);
           }
         });
       }
     });
   }
   
-  
-  private void androidPatch1() {
-    // 21/10/2015 WORKAROUND: a volte il picker non compare, con questo sembra che funzioni meglio (non togliere le get) 
-    if (applyAndroidPatch1) {
-      GwtUtils.deferredExecution(500, new Delegate<Void>() {
-        public void execute(Void element) {
-          onAvailableElementByClassname("picker", new Delegate<Element>() {
-            public void execute(Element element) {
-//            String msg = "Found picker element " + element.getClassName() + " " + element.getAbsoluteTop() + " " + element.getAbsoluteLeft() + " " + element.getClientHeight() + " " + element.getClientWidth() + " " + element.getStyle().getVisibility();
-              if (element.getClassName().contains("focus")) {
-                element.focus();
-//              PhgUtils.log("focusing picker element");
-              }
-              //PhgUtils.log(msg);
-            }
-          });
-        }
-      });
-    }
-  }
-  
-  private static void onAvailableElementByClassname (final String classname, final Delegate<Element> delegate) {
-    new Timer() {
-      public void run() {
-        Element element = JQuery.selectFirstElement("." + classname);
-        if (element != null) {
-//        this.cancel();
-          delegate.execute(element);
-        } else {
-          PhgUtils.log("Picker element timer cancel");
-          this.cancel();
-        }
-      }
-    }.scheduleRepeating(1000);
-  }
-  
-  private void androidPatch2() {
-    if (applyAndroidPatch2) {
-      OnsenUi.onAvailableElement(getElement(), new Delegate<Element>() {
-        public void execute(Element dateboxElement) {
-          OnsEventUtils.addHandler(dateboxElement, "touchend", true, new NativeGestureHandler() {
-            public void on(NativeGestureEvent event) {
-              PhgUtils.log("OnsDateBox.touchend");
-              GwtUtils.deferredExecution(100, new Delegate<Void>() {
-                public void execute(Void v) {
-                  Element pickerHolderElement = JQuery.selectFirstElement(".picker__holder");
-                  if (pickerHolderElement != null) {
-                    GwtUtils.setJsPropertyString(pickerHolderElement.getStyle(), "transition", "initial");
-                    PhgUtils.log("OnsDateBox.touchend: set transition initial on picker_holder");
-                  }
-                }
-              });
-            }
-          });
-        }
-      });
-    }
-  }
   
 }
